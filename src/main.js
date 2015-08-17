a43a23bd4ce6b8b64dc03925496590e325afee99
Patrick Gallagher <paddy.gallagher@gmail.com>
2015-08-17 18:08:59 +0100

    Adding a new About page and basic routing using hash change

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 $ = jQuery = require('jquery');
 var React = require('react');
-var HomePage = require('./components/pages/Home');
+var Home = require('./components/pages/Home');
+var About = require('./components/pages/About');
 
-React.render(<HomePage />, document.getElementById('app'));
+var App = React.createClass({
+  render: function () {
+    var Child;
+    switch (this.props.route) {
+      case 'about': Child = About; break;
+      default: Child = Home;
+    }
+    return (
+        <div>
+          <Child/>
+        </div>
+    );
+  }
+});
+
+function render() {
+  var route = window.location.hash.substr(1);
+  React.render(<App route={route} />, document.getElementById('app'));
+}
+
+window.addEventListener('hashchange', render);
+render();
