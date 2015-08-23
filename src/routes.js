6cf6787797aa1f8383b6ad3fa648a8b2ddb8b01b
Patrick Gallagher <paddy.gallagher@gmail.com>
2015-08-23 22:57:53 +0100

    Adding redirect

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
 var Route = Router.Route;
 
 var routes = (
@@ -10,6 +11,7 @@ var routes = (
       <Route name="users" handler={require('./components/pages/User')} />
       <Route name="about" handler={require('./components/pages/About')} />
       <NotFoundRoute handler={require('./components/pages/NotFound')} />
+      <Redirect from="about*" to="about" />
     </Route>
 );
 
